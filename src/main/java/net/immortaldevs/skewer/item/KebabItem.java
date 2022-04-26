fc6afb8af91891424db83691699572401ea24c28
Jack Papel <jackpapel@live.com>
2022-04-25 19:47:21 -0700

    Move Mixin, Add tooltips

--- src/main/java/net/immortaldevs/skewer/item/KebabItem.java
+++ src/main/java/net/immortaldevs/skewer/item/KebabItem.java
@@ -1,11 +1,18 @@
 package net.immortaldevs.skewer.item;
 
+import net.immortaldevs.sar.api.SkeletalComponentData;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class KebabItem extends Item {
     public KebabItem(Settings settings) {
@@ -18,5 +25,20 @@ public class KebabItem extends Item {
         return user instanceof PlayerEntity player && player.getAbilities().creativeMode
                 ? result
                 : new ItemStack(Items.STICK);
+    }
+
+    /**
+     * Display skewered items on the tooltip
+     */
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        if (!context.isAdvanced()) return;
+        SkeletalComponentData kebab = stack.getComponent("kebab");
+        if (kebab == null) return;
+
+        SkeletalComponentData.Children foods = kebab.getChildren("foods");
+        for (int i = 0; i < foods.size(); i++) {
+            tooltip.add(new LiteralText(foods.get(i).getComponent().getId().toString()));
+        }
     }
 }
---
+++ src/main/java/net/immortaldevs/skewer/mixin/ItemMixin.java
@@ -0,0 +1,55 @@
+package net.immortaldevs.skewer.mixin;
+
+import net.immortaldevs.sar.api.Component;
+import net.immortaldevs.sar.api.SkeletalComponentData;
+import net.immortaldevs.skewer.component.KebabComponent;
+import net.immortaldevs.skewer.component.SkewerComponents;
+import net.immortaldevs.skewer.component.food.SkewerableFoods;
+import net.immortaldevs.skewer.item.SkewerItems;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.StackReference;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.util.ClickType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Item.class)
+public class ItemMixin {
+
+    /**
+     * Allows items to be put onto sticks and skewers when clicked with a stick or skewer
+     * in the inventory
+     * @param stack The ItemStack of this item (the food, presumably)
+     * @param otherStack The ItemStack of the skewer / stick
+     * @param cursorStackReference Also the skewer / stick, but mutable. 🤷‍♀️
+     */
+    @Inject(method = "onClicked", at = @At("RETURN"), cancellable = true)
+    public void onClicked(ItemStack stack, ItemStack otherStack, Slot slot, ClickType clickType, PlayerEntity player, StackReference cursorStackReference, CallbackInfoReturnable<Boolean> cir) {
+        if (cursorStackReference.get().getCount() > 1) return;
+        Component component = SkewerableFoods.get((Item) (Object) this);
+        if (component == null) return;
+
+        if (cursorStackReference.get().getItem() == Items.STICK) {
+            cursorStackReference.set(new ItemStack(SkewerItems.KEBAB,
+                    cursorStackReference.get().getCount()));
+        }
+
+        if (cursorStackReference.get().getItem() == SkewerItems.KEBAB) {
+            SkeletalComponentData kebab = cursorStackReference.get().getComponent("kebab");
+            if (kebab == null) {
+                kebab = cursorStackReference.get().getOrCreateComponent("kebab", SkewerComponents.KEBAB);
+                kebab.getOrCreateNbt().putDouble("posY", -0.0625);
+            }
+            if (kebab.getChildren("foods").size() < 6) {
+                KebabComponent.addFood(kebab, component);
+                if (!player.isCreative()) stack.decrement(1);
+                cir.setReturnValue(true);
+            }
+        }
+    }
+}
